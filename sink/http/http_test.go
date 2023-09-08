7dfed0357eb06e255cbe60191e195c4d460d8f3c
Muhammad Zaki Al-Afrani <zaki.afrani@gmail.com>
2023-09-08 22:24:20 +0700

    add some basic options and test

--- sink/http/http_test.go
+++ sink/http/http_test.go
@@ -1,8 +1,43 @@
 package http
 
-func ExampleNewHTTP() {
-	// Create a new HTTP sink.
-	_, _ = NewClient()
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
 
-	// Output:
+func TestHTTPWrite(t *testing.T) {
+	expected := "PONG!"
+
+	svr := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, expected)
+	}))
+	defer svr.Close()
+
+	c, err := NewClient(svr.URL, http.MethodPost,
+		WithTimeout(1*time.Second),
+		WithHeader(map[string]interface{}{
+			"key": "value",
+		}),
+		WithRetry(RetryConfig{
+			MaxRetries:      3,
+			Backoff:         2 * time.Second,
+			RetryStatusCode: []int{500, 502},
+		}),
+	)
+	if err != nil {
+		t.Errorf("expected err to be nil got %v", err)
+	}
+
+	res, err := c.Write(context.Background(), []byte("PING"))
+	if err != nil {
+		t.Errorf("expected err to be nil got %v", err)
+	}
+
+	if string(res) != expected {
+		t.Errorf("expected res to be %s got %s", expected, res)
+	}
 }
